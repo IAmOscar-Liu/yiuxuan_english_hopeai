b1c7fd3174a90533e29ede64622c23dfeeb82fc4
Oscar Liu <oscar@symptomtrace.com>
2026-03-15 20:32:59 +0800

    remove comment in firebase-admin.ts

--- src/firebase-admin.ts
+++ src/firebase-admin.ts
@@ -5,10 +5,6 @@ import path from "path";
 console.log(`credential: ${process.env.FIREBASE_CREDENTIAL_FILE}`);
 console.log(`storageBucket: ${process.env.FIREBASE_BUCKET}`);
 
-// Import your service account key JSON file
-// Ensure the path is correct relative to this file
-// const serviceAccount = require("../../flutter-ad-ecommerce-dev-5d9da-firebase-adminsdk-fbsvc-d73658c022.json");
-
 if (!process.env.FIREBASE_CREDENTIAL_FILE) {
   throw new Error("FIREBASE_CREDENTIAL_FILE is not defined");
 }
@@ -21,7 +17,6 @@ const serviceAccountPath = path.resolve(
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountPath),
   storageBucket: process.env.FIREBASE_BUCKET,
-  // storageBucket: "flutter-ad-commerce.firebasestorage.app",
 });
 
 const bucket = admin.storage().bucket();
